5210c687af271c317f7001736190e3189064405f
Alex Apanassik <alex.apanasik@gmail.com>
2021-12-20 22:20:22 +0300

    add functions

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let guessesArray = ["Rock", "Papers", "Scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
     let randomIndex = Math.floor(Math.random() * 3);
@@ -26,49 +28,57 @@ function playRound(computerSelection, playerSelection) {
         (computerSelection === "Scissors" && playerSelection === "Papers") ||
         (playerSelection === "Rock" && computerSelection === "Papers") ||
         (playerSelection === "Scissors" && computerSelection === "Rock")
-    ) return "Loose";
-    
+    ) return "Loose"; 
     // papers - papers  // scissors-scissors  // rock-rock -> Tie
     else return "Tie";
 }
 
-function game() {
-    let playerScore = 0,
-        computerScore = 0,
-        roundResult,
-        computerSelection,
-        playerSelection;
+function showRoundResult(roundResult, computerSelection, playerSelection) {
+    console.log("Computer choose: ", computerSelection);
+    console.log("You choose: ", playerSelection);
+    console.log('---------------------------------');
+    console.log("Round Result: ", roundResult);
+    console.log('    ');
+}
 
-    for (var i = 0; i < 5; i++) {
-    
-        computerSelection = computerPlay(); // computer choice
-        playerSelection = getPlayerSelection(); // player choice
-        
-        if (playerSelection == null) return; // cancel game
-
-        // play round
-        roundResult = playRound(computerSelection, playerSelection);
-        // report round result
-        console.log("Computer choose: ", computerSelection);
-        console.log("You choose: ", playerSelection);
-        console.log('---------------------------------');
-        console.log("Round Result: ", roundResult);
-        console.log('    ');
-        
-        // add result to total score
-        if (roundResult == "Won") {
-            playerScore += 1;
-        } else if (roundResult == "Loose") {
-            computerScore += 1;
-        }
+function gameResult(roundResult) {
+    if (roundResult == "Won") {
+        playerScore += 1;
+    } else if (roundResult == "Loose") {
+        computerScore += 1;
     }
-    // report total game score
+}
+
+function showGameResult(computerScore, playerScore) {
     console.log("Total score of player: ", playerScore);
     console.log("Total score of computer: ", computerScore);
     console.log('==========================================');
     if (playerScore > computerScore) console.log("Game result: You won!");
     else if (playerScore < computerScore) console.log("Game result: You loose!");
     else console.log('Game result: Tie');
+}
+
+function game() {
+let roundResult,
+    computerSelection,
+    playerSelection;
+
+    for (var i = 0; i < 5; i++) {
+        // computer choice
+        computerSelection = computerPlay(); 
+        // player choice
+        playerSelection = getPlayerSelection(); 
+        // player cancel game
+        if (playerSelection == null) return; 
+        // play round
+        roundResult = playRound(computerSelection, playerSelection);
+        // show round result
+        showRoundResult(roundResult, computerSelection, playerSelection);
+        // add round result to total game score
+        gameResult(roundResult);
+    }
+    // show total game score
+    showGameResult(computerScore, playerScore);
 }
 
 game();
